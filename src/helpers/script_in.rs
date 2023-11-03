ff9b7ccaeac76b94e71bb5b33145cbef6800c1ed
tm-ahad <tm.ahad.07@gmail.com>
2023-11-03 23:25:21 +0600

    Debugging.

--- src/helpers/script_in.rs
+++ src/helpers/script_in.rs
@@ -1,7 +1,7 @@
 use crate::consts::{SBF, SBS};
 
 pub fn parse_dyn_component(script: &str, html: &str) -> String {
-    format!("(() => {SBF}{script} return `{html}`\n{SBS}())")
+    format!("(() => ({SBF}{script} return `{html}`\n{SBS})())")
 }
 
 pub fn parse_stat_component_script(dom_script: &str) -> String {
