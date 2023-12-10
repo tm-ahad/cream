2644813597f2920719e22eff6344f69b4db26922
tm-ahad <tm.ahad.07@gmail.com>
2023-12-10 21:32:01 +0600

    src/out.rs updated

--- src/helpers/create_file.rs
+++ src/helpers/create_file.rs
@@ -1,14 +1,24 @@
+#[cfg(not(windows))]
 use std::os::unix::fs::PermissionsExt;
-use std::fs::{OpenOptions, self};
+#[cfg(windows)]
+use std::os::windows::fs::OpenOptionsExt;
+
+use std::fs::{self, OpenOptions};
 
 pub fn create_file(path: String) {
-    let _ = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(&path);
+    let mut options = OpenOptions::new();
+    options.create(true).write(true);
 
-    // Set file permissions after creating the file
-    if let Err(e) = fs::set_permissions(&path, fs::Permissions::from_mode(0o777)) {
-        panic!("{e}")
+    #[cfg(windows)]
+    {
+        options.access_mode(0o666); // Permissions for Windows
+    }
+
+    let _ = options.open(path.clone());
+
+    #[cfg(not(windows))]
+    {
+        let permissions = fs::Permissions::from_mode(0o777); // Permissions for Unix-based platforms
+        let _ = fs::set_permissions(path, permissions);
     }
 }
