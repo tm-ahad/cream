952f0a93a260c7f13e0209e0213483b78fe5c2ff
tm-ahad <tm.ahad.07@gmail.com>
2024-05-15 08:07:16 +0600

    src/helpers/create_file.rs fixed for macos.

--- src/helpers/create_file.rs
+++ src/helpers/create_file.rs
@@ -1,10 +1,10 @@
 use crate::std_err::{ErrType, StdErr};
+use std::fs::{File, OpenOptions, self};
+
 #[cfg(not(windows))]
 use std::os::unix::fs::PermissionsExt;
-use std::fs;
 #[cfg(windows)]
 use std::os::windows::fs::OpenOptionsExt;
-use std::fs::{File, OpenOptions};
 
 pub fn create_file(path: String) -> File {
     let mut options = OpenOptions::new();
@@ -18,7 +18,7 @@ pub fn create_file(path: String) -> File {
     #[cfg(not(windows))]
     {
         let permissions = fs::Permissions::from_mode(0o777); // Permissions for Unix-based platforms
-        let _ = fs::set_permissions(path, permissions);
+        let _ = fs::set_permissions(path.clone(), permissions);
     }
 
     return options.open(path.clone()).unwrap_or_else(|e| {
